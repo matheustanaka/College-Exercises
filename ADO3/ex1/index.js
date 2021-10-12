3ff70935dadce0d684e70ba80d98a259c8cae332
Matheus Tanaka <matheustanakanog@gmail.com>
2021-10-11 22:27:18 -0300

    feat: add comments

--- ADO3/ex1/index.js
+++ ADO3/ex1/index.js
@@ -1,15 +1,18 @@
+//const to get our form.
 const form = document.getElementById('form');
+//adding a event listener when our submit is clicked.
 form.addEventListener('submit', handleSubmit);
-
+//a function to handle submit passing a event as parameter.
 function handleSubmit (event) {
+    //if our event does not get explicitly handled, its default action should not be taken as it normally would be.
     event.preventDefault();
-
+    //getting temp and result inside of our form
     const temp = getInputNumberValue('temp');
     const result = document.getElementById('result');
 
-
+    //calculate degress
     const farenheit = (temp * (1.8)) + 32;
-
+    //creating a layout with our result
     const layout = `
 
     <h1>Aqui está o Resultado</h1>
@@ -19,7 +22,7 @@ function handleSubmit (event) {
     result.innerHTML = layout;
 
 }
-
+//adding a function to get a number inside our input and passing an id as parameter and return an id with a value.
 function getInputNumberValue(id) {
     return Number(document.getElementById(id).value);
 }
