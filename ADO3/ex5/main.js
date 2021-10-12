57a51e59e24ddebe8112514146f4d7c660f93540
Matheus Tanaka <matheustanakanog@gmail.com>
2021-10-11 22:17:38 -0300

    feat: add comments

--- ADO3/ex5/main.js
+++ ADO3/ex5/main.js
@@ -1,7 +1,8 @@
+//Function to set new image
 function setNewImg(){
     document.getElementById('img1').src = "./img/19.jpg"
 }
-
+//Function to set old image
 function setOldImg(){
     document.getElementById('img1').src = "./img/14.jpg"
 }
