66eafa4a94d63436d08183095ccfc5589b19045c
Matheus Tanaka <matheustanakanog@gmail.com>
2022-01-27 17:39:28 -0300

    feat: solve first exercise

---
+++ js-exercises/mediaAritimetica.js
@@ -0,0 +1,8 @@
+//Ler duas notas e imprimir a média aritimética.
+
+const pergunta1 = Number(prompt("Digite um número: "));
+const pergunta2 = Number(prompt("Digite um número: "));
+
+const media = (pergunta1 + pergunta2) / 2;
+
+console.log(media);
