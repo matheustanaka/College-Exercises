0e09fa0d37f95677e05ec87cf7ec5c04359a58e1
Matheus Tanaka <matheustanakanog@gmail.com>
2022-01-27 19:08:25 -0300

    feat: add new solution for exercise

---
+++ js-exercises/imparOuPar.js
@@ -0,0 +1,11 @@
+//Escreva um programa que pede um inteiro e imprime um número 0 ou 1, 0 se for par e 1 se for impar
+
+const inteiro = 4;
+
+if(inteiro%2 == 0 ) {
+  console.log("par "+0);
+ 
+}
+else {
+  console.log("impar "+ 1);
+}
