7fe40ead70175e750de4075b57d24ddb6d6c3707
Matheus Tanaka <matheustanakanog@gmail.com>
2022-02-07 16:31:21 -0300

    feat: add new solution for exercise

---
+++ js-exercises/condicionais/ordemCrescente.js
@@ -0,0 +1,10 @@
+//Agora seu programa deve receber dois números e exibir eles em ordem crescente
+
+const num1 = 10;
+const num2 = 5;
+
+if (num1 > num2) {
+  console.log(num1 + " " + num2);
+} else {
+  console.log(num2 + " " + num1);
+}
