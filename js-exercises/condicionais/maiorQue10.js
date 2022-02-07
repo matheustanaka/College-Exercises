ee7cf40dc22c2c24ab52b0909bbb84d944ae0a83
Matheus Tanaka <matheustanakanog@gmail.com>
2022-02-07 16:24:09 -0300

    feat: add new solution for exercise

---
+++ js-exercises/condicionais/maiorQue10.js
@@ -0,0 +1,9 @@
+//Faça um algoritmo que receba um número e mostre uma mensage caso este número seja maior que 10.
+
+const input = Number(prompt("Digite um valor:"));
+
+if (input > 10) {
+  console.log("Este número é MAIOR do que 10");
+} else {
+  console.log("Este número é MENOR do que 10");
+}
