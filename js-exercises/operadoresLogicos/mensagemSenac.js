cf7d249541fbbf1488c0bf9d116823833627560d
Matheus Tanaka <matheustanakanog@gmail.com>
2022-02-07 22:58:16 -0300

    feat: add new solution

---
+++ js-exercises/operadoresLogicos/mensagemSenac.js
@@ -0,0 +1,13 @@
+//Escreva um programa que solicite um numero e exibe a mensagem "SENAC" se este numero for 13, 26, 42 ou 70.
+
+const number = Number(prompt("DIgite um número: "));
+
+if(number == 13) {
+  console.log("SENAC")
+}
+else if(number == 26) {
+  console.log("SENAC")
+}
+else if(number == 42 || number == 70) {
+  console.log("SENAC")
+}
